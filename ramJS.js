6dd673cdfc3fa5c3330e27811a1df006a40cf0ed
9000J <9000janakiram@gamail.com>
2022-12-27 15:17:15 +0530

    kkk

---
+++ ramJS.js
@@ -0,0 +1,12 @@
+function fnAddtion(){
+
+ var val= document.getElementById('value').innerHTML++
+ document.getElementById('value').innerHTML=++val
+
+}
+function fnSubtraction(){
+    //document.getElementById("value").innerHTML--
+ var val= document.getElementById('value').innerHTML
+ document.getElementById('value').innerHTML=--val
+ 
+}
